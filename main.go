bec4b8469ee3a893904a53de675b8f8f4a5f8187
Markelov A <mrklv001@gmail.com>
2025-08-24 22:27:38 +0300

    Update func main

--- main.go
+++ main.go
@@ -1,22 +1,16 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
 func main() {
-	var random float64 = 44.4
-	isEven := random == math.Trunc(random) && int(random)%2 == 0
+	var n, c, d int = 20, 3, 5
+	fmt.Scan(&n, &c, &d)
+	// Вывести первое число от 1 до n включительно, кратное c, но НЕ кратное d. Если такого числа нет - выводить ничего не нужно.
+	for i := 1; i <= n; i++ {
+		if i%c == 0 && i%d != 0 {
+			fmt.Println(i)
+			break
 
-	fmt.Printf("Исходное число: %.1f\n", random)
-	fmt.Printf("Исходное число, увеличенное на 10%%: %.5f\n", random+(random*0.1))
-	fmt.Printf("Исходное число является четным: %t\n", isEven)
-	if random < 10 {
-		fmt.Println("Предпоследняя цифра целой части исходного числа: 0")
-	} else if 9 < random && random < 100 {
-		fmt.Printf("Предпоследняя цифра целой части исходного числа: %d\n", int(random)/10)
-	} else if random > 99 {
-		fmt.Printf("Предпоследняя цифра целой части исходного числа: %d\n", int(random)/100)
+		}
 	}
 }
